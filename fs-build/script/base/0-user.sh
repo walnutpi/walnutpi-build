406f7617327639c8ab2b23ddc677729786782c39
sc-bin <3335447573@qq.com>
2023-10-13 17:10:44 +0800

    取消sudo密码 & 修改Python实现方式

--- fs-build/script/base/0-user.sh
+++ fs-build/script/base/0-user.sh
@@ -11,8 +11,12 @@ useradd -m pi
 echo "pi:pi" |  chpasswd
 usermod -aG sudo pi
 cp .bashrc /home/pi/
-echo "Defaults rootpw" |  tee -a /etc/sudoers
 sed -i 's|/home/pi:/bin/sh|/home/pi:/bin/bash|g' /etc/passwd
+
+echo "Defaults rootpw" |  tee -a /etc/sudoers
+echo "pi ALL=(ALL) NOPASSWD:ALL" |  tee -a /etc/sudoers
+echo "Defaults !env_reset" |  tee -a /etc/sudoers
+
 
 # 赋予权限
 chown pi:pi /home/pi/
@@ -22,4 +26,5 @@ chown root:root /usr/bin/sudo
 chmod 4755 /usr/bin/sudo
 
 rm .bashrc
+
 
