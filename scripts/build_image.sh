6883a7ce91352cdf795afd8682f047155b9962c0
sc-bin <3335447573@qq.com>
2025-01-03 10:26:56 +0800

    优化构建镜像时解除系统占用的脚本

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -33,72 +33,71 @@ check_resource() {
 
 build_image() {
     cleanup() {
+        local LOOP_DEVICE=$1
         echo "Cleaning up..."
-        
+        cd $PATH_PROJECT_DIR
         if mountpoint -q "$TMP_mount_disk2/boot"; then
             echo "Unmounting $TMP_mount_disk2/boot"
-            umount "$TMP_mount_disk2/boot"
+            while mountpoint -q "$TMP_mount_disk2/boot"; do
+                umount "$TMP_mount_disk2/boot"
+                sleep 1
+            done
         fi
-        
-        # 检查并卸载 $TMP_mount_disk1
         if mountpoint -q "$TMP_mount_disk1"; then
             echo "Unmounting $TMP_mount_disk1"
-            umount "$TMP_mount_disk1"
+            while mountpoint -q "$TMP_mount_disk1"; do
+                umount "$TMP_mount_disk1"
+                sleep 1
+            done
         fi
-        
-        # 检查并卸载 $TMP_mount_disk2
         if mountpoint -q "$TMP_mount_disk2"; then
             echo "Unmounting $TMP_mount_disk2"
-            umount "$TMP_mount_disk2"
+            while mountpoint -q "$TMP_mount_disk2"; do
+                umount "$TMP_mount_disk2"
+                sleep 1
+            done
         fi
         
-        # 检查并删除 kpartx 设备
-        if kpartx -l | grep -q "$LOOP_DEVICE"; then
-            echo "Deleting kpartx devices for $LOOP_DEVICE"
-            kpartx -dv "$LOOP_DEVICE"
-        else
-            echo "No kpartx devices found for $LOOP_DEVICE"
+        if [ ! -z "$LOOP_DEVICE" ]; then
+            if losetup -l > /dev/null 2>&1; then
+                while losetup -l | grep -q "$LOOP_DEVICE"; do
+                    echo "Releasing loop device $LOOP_DEVICE"
+                    kpartx -dv "$LOOP_DEVICE"
+                    losetup -d "$LOOP_DEVICE"
+                    sleep 1
+                done
+            fi
         fi
-        
-        # 检查并释放 loop 设备
-        if losetup -l | grep -q "$LOOP_DEVICE"; then
-            echo "Releasing loop device $LOOP_DEVICE"
-            losetup -d "$LOOP_DEVICE"
-        else
-            echo "Loop device $LOOP_DEVICE is not in use"
-        fi
-        
-        exit 1
     }
-    trap cleanup SIGINT
+    
+    trap 'cleanup "$LOOP_DEVICE"; exit' SIGINT
     check_resource
     if [  -d $TMP_mount_disk1 ]; then
-        umount $TMP_mount_disk1
         rm -r $TMP_mount_disk1
     fi
     if [ -d $TMP_mount_disk2 ]; then
-        umount $TMP_mount_disk2
         rm -r $TMP_mount_disk2
     fi
     mkdir -p $TMP_mount_disk1
     mkdir -p $TMP_mount_disk2
     
     cd ${PATH_SOURCE}/wpi-update
-    local VERSION_APT=$(echo $(./wpi-update -s | tail -n 1 ))
-    
+    echo -n "$BOARD_MODEL" > /tmp/walnutpi-board_model
+    VERSION_APT=$(echo $(./wpi-update -s | tail -n 1 ))
+    reload_env
     if [ -f "$OUT_IMG_FILE" ]; then
         rm ${OUT_IMG_FILE}
     fi
     
     local ROOTFS_SIZE=$(du -sm $TMP_rootfs_build | cut -f1)
-    local PART2_SIZE=$((ROOTFS_SIZE + 500))
+    local PART2_SIZE=$((ROOTFS_SIZE + 800))
     
     echo "PART1_SIZE=${PART1_SIZE}MB"
     echo "PART2_SIZE=${PART2_SIZE}MB"
     
     # 创建img文件
     IMG_SIZE=$((PART1_SIZE + PART2_SIZE + 2 ))
-    run_status "create img file: $OUT_IMG_FILE " dd if=/dev/zero of=$OUT_IMG_FILE bs=1M count=$IMG_SIZE
+    run_status "create img file: $OUT_IMG_FILE $IMG_SIZE MB" dd if=/dev/zero of=$OUT_IMG_FILE bs=1M count=$IMG_SIZE
     
     # 创建分区
     echo "创建分区"
@@ -176,13 +175,8 @@ build_image() {
     echo ""generate initramfs""
     run_as_silent chroot $TMP_mount_disk2 /bin/bash -c "DEBIAN_FRONTEND=noninteractive  update-initramfs -uv -k $kernel_version"
     
-    
-    umount $TMP_mount_disk2/boot
-    umount $TMP_mount_disk1
-    umount $TMP_mount_disk2
-    kpartx -dv $LOOP_DEVICE
-    losetup -d $LOOP_DEVICE
     trap - SIGINT EXIT
+    cleanup "$LOOP_DEVICE"
     
     current_hour=$(date +"%H")
     current_minute=$(date +"%M")
